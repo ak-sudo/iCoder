70f707511b3fbba61a5c523912bb14c6be58e8ee
ak-sudo <72704561+ak-sudo@users.noreply.github.com>
2020-10-13 19:36:43 +0530

    Add files via upload

---
+++ bootstrap/css/style.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+    <title></title>
+    <style type="text/css">
+
+*
+{
+    margin: 0;
+    padding: 0;
+    font-family: 'Bree Serif', serif;
+    box-sizing: border-box;
+}
+
+.divider-text
+{
+    position: relative;
+    text-align: center;
+    margin-top: 15px;
+    margin-bottom: 15px;
+}
+
+.driver-text span
+{
+    padding: 7px;
+    font-size: 12px;
+    position: relative;
+    z-index: 2;
+}
+
+.driver-text::after
+{
+    content: "";
+    position: absolute;
+    width: 100%;
+    border-bottom: 1px solid #ddd;
+    top: 55%;
+    left: 0;
+    z-index: 1;
+}
+
+.btn-facebook
+{
+    background-color: #405D9D!important;
+    color: #fff!important;
+}
+
+.btn-gmail
+{
+    background-color: #ea4335!important;
+    color: #fff!important;
+}
+
+.text-center
+{
+    text-align: center;
+}
+
+.card-title-mt-3-text-center
+{
+    font-weight: bold;
+    text-align: center;
+}
+   </style>
+</head>
+<body>
+
+</body>
+</html>
+
---
+++ bootstrap/links/links.php
@@ -0,0 +1,8 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet"> 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  
